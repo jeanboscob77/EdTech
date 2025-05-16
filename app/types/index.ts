decb89893f786c5d06a9e64a70428aa3a3768bee
Jean Bosco B <jeanboscob77@gmail.com>
2025-05-16 12:13:40 +0200

    an interface for reduxProvider added

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,3 +1,5 @@
+import { ReactNode } from "react";
+
 export type Course = {
   _id: string;
   title: string;
@@ -7,19 +9,22 @@ export type Course = {
   prerequisites?: string;
   instructor: string;
   isEnrolled: boolean;
-  error: string
+  error: string;
 };
 
-export interface EnrollButtonProps{
-  courseId: string,
+export interface EnrollButtonProps {
+  courseId: string;
 }
 
 export interface ExtendedEnrollButtonProps extends EnrollButtonProps {
   isEnrolled: boolean;
-  buttonText?: string; // Optional now, we compute internally
+  buttonText?: string;
 }
-
 
 export type Props = {
   initialCourses: Course[];
 };
+
+export interface ReduxProviderProps {
+  children: ReactNode;
+}
