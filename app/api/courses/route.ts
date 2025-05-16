0adb2a7ac0d504d209d7be855776b6879113aaa1
Jean Bosco B <jeanboscob77@gmail.com>
2025-05-16 12:25:33 +0200

    route for handling coures by userId

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,6 +1,7 @@
 import { connectDB } from "@/lib/db";
 import Course from "@/lib/models/courses";
 import { NextResponse } from "next/server";
+import mongoose from "mongoose";
 
 export async function GET(req: Request) {
   try {
@@ -9,13 +10,30 @@ export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
     const id = searchParams.get("id");
     const search = searchParams.get("search");
+    const userId = searchParams.get("userId");
 
     if (id) {
       const course = await Course.findById(id);
       if (!course) {
-        return NextResponse.json({ message: "Course not found" }, { status: 404 });
+        return NextResponse.json(
+          { message: "Course not found" },
+          { status: 404 }
+        );
       }
       return NextResponse.json(course);
+    }
+
+    if (userId) {
+      const courses = await Course.find({
+        userId: new mongoose.Types.ObjectId(userId),
+      });
+      if (!courses) {
+        return NextResponse.json(
+          { message: "You have not enrolled in any course!!!" },
+          { status: 404 }
+        );
+      }
+      return NextResponse.json(courses);
     }
 
     const filter = search
@@ -28,11 +46,14 @@ export async function GET(req: Request) {
       : {};
 
     const courses = await Course.find(filter);
-    console.log(courses)
+    console.log(courses);
     return NextResponse.json(courses);
   } catch (error) {
     console.error("Error in GET:", error);
-    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
 
@@ -41,10 +62,29 @@ export async function POST(req: Request) {
     await connectDB();
 
     const body = await req.json();
-    const { title, shortDescription, fullDescription, duration, prerequisites, instructor, isEnrolled, userId} = body;
+    const {
+      title,
+      shortDescription,
+      fullDescription,
+      duration,
+      prerequisites,
+      instructor,
+      isEnrolled,
+      userId,
+    } = body;
 
-    if (!title || !shortDescription || !fullDescription || !duration || !prerequisites || !instructor) {
-      return NextResponse.json({ message: "All fields are required" }, { status: 400 });
+    if (
+      !title ||
+      !shortDescription ||
+      !fullDescription ||
+      !duration ||
+      !prerequisites ||
+      !instructor
+    ) {
+      return NextResponse.json(
+        { message: "All fields are required" },
+        { status: 400 }
+      );
     }
 
     const newCourse = new Course({
@@ -55,17 +95,19 @@ export async function POST(req: Request) {
       prerequisites,
       instructor,
       isEnrolled,
-      userId
+      userId,
     });
 
     await newCourse.save();
     return NextResponse.json(newCourse, { status: 201 });
   } catch (error) {
     console.error("Error in POST:", error);
-    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
-
 
 export async function PATCH(req: Request) {
   try {
@@ -75,29 +117,41 @@ export async function PATCH(req: Request) {
     const id = searchParams.get("id");
 
     if (!id) {
-      return NextResponse.json({ message: "Course ID is required" }, { status: 400 });
+      return NextResponse.json(
+        { message: "Course ID is required" },
+        { status: 400 }
+      );
     }
 
     const body = await req.json();
-    const { isEnrolled, userId} = body;
+    const { isEnrolled, userId } = body;
 
-    if (isEnrolled === undefined ||!userId) {
-      return NextResponse.json({ message: "Enroll and UserId are required" }, { status: 400 });
+    if (isEnrolled === undefined || !userId) {
+      return NextResponse.json(
+        { message: "Enroll and UserId are required" },
+        { status: 400 }
+      );
     }
 
     const updatedCourse = await Course.findByIdAndUpdate(
       id,
-      { isEnrolled, userId},
+      { isEnrolled, userId },
       { new: true }
     );
 
     if (!updatedCourse) {
-      return NextResponse.json({ message: "Course not found" }, { status: 404 });
+      return NextResponse.json(
+        { message: "Course not found" },
+        { status: 404 }
+      );
     }
 
     return NextResponse.json(updatedCourse);
   } catch (error) {
     console.error("Error in PATCH:", error);
-    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
